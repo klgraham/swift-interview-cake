d4fe1586d6208403d66dcc07247957c37bd3dce8
Kenneth Graham <kenneth.graham@digitalreasoning.com>
2016-07-18 10:42:36 -0500

    changed riffle shuffle function so that it takes a deck as input and returns a riffled deck. Before it took two halves of the deck as input.

--- single-riffle-shuffle.playground/Contents.swift
+++ single-riffle-shuffle.playground/Contents.swift
@@ -1,32 +1,30 @@
 import Foundation
 
 let unshuffledDeck = (0..<52).map { $0 }
-let half1 = unshuffledDeck.filter { $0 < 52/2 }
-let half2 = unshuffledDeck.filter { $0 >= 52/2 }
 
 // an integer on [0, max]
 func randomInt(max: Int) -> Int {
     return Int(arc4random_uniform(UInt32(max + 1)))
 }
 
-func riffleShuffle(deck1: [Int], deck2: [Int]) -> [Int] {
+func riffleShuffle(deck: [Int]) -> [Int] {
+    var half1 = deck.filter { $0 < 52/2 }
+    var half2 = deck.filter { $0 >= 52/2 }
     var shuffledDeck = [Int]()
-    var deck1Copy = deck1
-    var deck2Copy = deck2
     
-    while (!deck1Copy.isEmpty || !deck2Copy.isEmpty) {
-        if !deck1Copy.isEmpty {
-            let cardsFromDeck1 = randomInt(max: deck1Copy.count)
+    while (!half1.isEmpty || !half2.isEmpty) {
+        if !half1.isEmpty {
+            let cardsFromDeck1 = randomInt(max: half1.count)
             for _ in 0..<cardsFromDeck1 {
-                let card = deck1Copy.remove(at: 0)
+                let card = half1.remove(at: 0)
                 shuffledDeck.append(card)
             }
         }
         
-        if !deck2Copy.isEmpty {
-            let cardsFromDeck2 = randomInt(max: deck2Copy.count)
+        if !half2.isEmpty {
+            let cardsFromDeck2 = randomInt(max: half2.count)
             for _ in 0..<cardsFromDeck2 {
-                let card = deck2Copy.remove(at: 0)
+                let card = half2.remove(at: 0)
                 shuffledDeck.append(card)
             }
         }
@@ -78,7 +76,7 @@ func deckIsRiffled(_ deck: [Int], domainSeparatationThreshold: Double = 3.0) -> 
 var riffledDeckCount = 0
 let N = 100
 for _ in 0...N {
-    let riffledDeck = riffleShuffle(deck1: half1, deck2: half2)
+    let riffledDeck = riffleShuffle(deck: unshuffledDeck)
     let isRiffled = deckIsRiffled(riffledDeck, domainSeparatationThreshold: 2.0)
     if isRiffled {
         riffledDeckCount += 1
